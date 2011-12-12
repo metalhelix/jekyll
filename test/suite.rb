c3526e859fa3e8cef7b2e64aa4c996e03d5311c0
Jim V <jfv@stowers.org>
2011-12-12 14:30:22 -0600

    ensure test dir in load path

--- test/suite.rb
+++ test/suite.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 
 # for some reason these tests fail when run via TextMate
 # but succeed when run on the command line.
-
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 tests = Dir[File.expand_path("#{File.dirname(__FILE__)}/test_*.rb")]
 tests.each do |file|
   require file
